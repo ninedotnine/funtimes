0c718251e944f545443f55f8b9aaf049402b3a9c
ninedotnine <warfegle@hotmail.com>
2013-05-27 22:36:44 -0400

    added conditional evaluation to pred format

--- predicaments.py
+++ predicaments.py
@@ -3,6 +3,7 @@
 # also the new home of the Predicament class
 
 import os
+from profiledata import profile
  
 predicaments = {}
 
@@ -34,6 +35,12 @@ class BadPredicamentError(Exception):
             print("no data directory")
         elif code == 9:
             print("%s has inputtype %s, which is insane." %(args[0],args[1]))
+        elif code == 10:
+            print("'%s' not in profile" % args[0])
+        elif code == 11:
+            print("unexpected line after", args[0])
+        elif code == 12:
+            print("unexpected end if in predicament", args[0])
         print("this is a fatal error. aborting")
         raise SystemExit
 
@@ -76,7 +83,7 @@ by checking the predicaments dictionary."""
                 if line.find('new predicament') != 0:
                     raise BadPredicamentError(1, self.name)
                 # if it's the wrong predicament, freak the hell out
-                if self.name != line.split('=')[1].strip():
+                elif self.name != line.split('=')[1].strip():
                     raise BadPredicamentError(2, self.name)
                 busy = True
                 break
@@ -85,14 +92,18 @@ by checking the predicaments dictionary."""
                 raise BadPredicamentError(5, filename)
 
             # finally, we start actually assigning the data
-            for line in fp:
-                line = line.strip()
-                # skip blank lines and lines starting with '#'
-                if not line or line[0] == '#': 
-                    continue
-                elif line.find("end of predicament") == 0:
+            # line should be true (it should still be the new pred line)
+            readingIfLevel = 0
+            while line:
+                line = getNonBlankLine(fp)
+                if line.find("end of predicament") == 0:
                     busy = False
                     break
+                elif line.find("end if") == 0:
+                    if readingIfLevel > 0:
+                        readingIfLevel -= 1
+                        continue
+                    raise BadPredicamentError(12, self.name)
                 key, value = line.split('=')
                 key = key.rstrip().lower()
                 if key == 'new predicament':
@@ -135,7 +146,7 @@ by checking the predicaments dictionary."""
                 elif key == 'next':
                     self.goto = value.strip()
                 elif key == 'inputtype':
-                    if value.strip() != 'none' and value.strip() != 'normal' and value.strip() != 'input':
+                    if value.strip() not in ('none', 'normal', 'input'):
                         print("WHOA! %s is not a chill inputtype!" % value)
                         raise BadPredicamentError(6, self.name)
                     self.inputtype = value.strip()
@@ -143,15 +154,63 @@ by checking the predicaments dictionary."""
                     self.result = value.strip()
                 elif key == 'prompt':
                     self.prompt = value.strip()
+                elif key[:2] == 'if':
+                    parameter = key.split()[1].strip()
+                    tempIfLevel = readingIfLevel + 1
+                    if doIf(fp, parameter, value.strip()):
+                        # if the condition is true, read normally
+                        readingIfLevel += 1
+                        continue
+                    # if the condition isn't true, 
+                    # discard lines until we reach end if
+                    while readingIfLevel < tempIfLevel:
+                        nextline = getNonBlankLine(fp)
+                        if nextline[:6] == 'end if':
+                            tempIfLevel -= 1
+                        elif nextline[:2] == 'if':
+                            tempIfLevel += 1
                 else:
                     print("%s is not a valid pred directive" % key)
                     raise BadPredicamentError(6, self.name)
-        if busy:
+        if busy or readingIfLevel:
             raise BadPredicamentError(7, self.name)
 
     # this isn't used anywhere, but handy for debugging
     def __str__(self):
         return 'Predicament: %s: %s' % (self.name, self.text)
+
+def doIf(fp, parameter, value):
+    # figures out whether to read conditional stuff in pred definitions
+    if parameter not in profile:
+        raise BadPredicamentError(10, parameter)
+    followup = getNonBlankLine(fp).lower()
+    conditionIsTrue = ( profile[parameter] == value.strip() )
+    if followup[:8] == 'then not':
+        # don't use this, it breaks if more than one statement is processed
+        # for the simplest statements, it's okay. 
+        return not conditionIsTrue
+    elif followup[:4] == 'then':
+        return conditionIsTrue
+    line = getNonBlankLine(fp)
+    if line[:3] != 'if ':
+        raise BadPredicamentError(11, "'%s'" % followup)
+    key, value = line.split('=')
+    parameter = key.split()[1].strip()
+    if followup[:7] == 'and not':
+        return ( not doIf(fp, parameter, value) and conditionIsTrue )
+    elif followup[:3] == 'and':
+        return ( doIf(fp, parameter, value) and conditionIsTrue )
+    elif followup[:6] == 'or not':
+        return ( not doIf(fp, parameter, value) or conditionIsTrue )
+    elif followup[:2] == 'or':
+        return ( doIf(fp, parameter, value) or conditionIsTrue )
+    raise BadPredicamentError(11, 'if %s = %s' % (parameter, value))
+
+def getNonBlankLine(fp):
+    line = fp.readline().strip()
+    while line == '' or line[0] == '#':
+        line = fp.readline().strip()
+    return line
 
 # populate predicaments dictionary with locations of all known predicaments
 for filename in os.listdir(datadir):
