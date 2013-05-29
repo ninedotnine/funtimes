6c02e3811394de42dec28a9c290eddcb640ce21d
ninedotnine <warfegle@hotmail.com>
2013-05-29 03:53:27 -0400

    small changes but make predicaments.py neater

--- predicaments.py
+++ predicaments.py
@@ -11,12 +11,6 @@ from settings import *
 #from stuffthatusedtobeinpredicaments import *
 from errors import errors
 
-predicaments = {}
-
-datadir = os.getcwd() + '/data/predicaments'
-if not os.path.isdir(datadir):
-    raise BadPredicamentError(8)
-
 class BadPredicamentError(Exception):
     def __init__(self, code=0, *args):
         print("\nerror code:", code)
@@ -321,23 +315,33 @@ def getNonBlankLine(fp):
     return line
 
 # populate predicaments dictionary with locations of all known predicaments
-for filename in os.listdir(datadir):
-    basename, ext = os.path.splitext(filename)
-    if ext != '.pred':
-        print("WARNING: skipping %s/%s%s..." % (datadir, basename, ext))
-        continue
-    busy = False # whether we're currently reading a predicament
-    lineNo = 0
-    with open(datadir + '/' + filename, 'r') as fp:
-        for line in fp:
-            lineNo += 1
-            line = line.strip()
-            if line.find("new predicament") == 0:
-                name = line.split('=')[1]
-                # create entry in predicaments dictionary
-                # 'title of predicament' : ('which pred file it is in',lineNo)
-                name = name.strip()
-                predicaments[name] = (filename, lineNo)
+def findPredicaments(datadir):
+    predicaments = {}
+    if not os.path.isdir(datadir):
+        raise BadPredicamentError(8)
+    for filename in os.listdir(datadir):
+        basename, ext = os.path.splitext(filename)
+        if ext != '.pred':
+            print("WARNING: skipping %s/%s%s..." % (datadir, basename, ext))
+            continue
+# ?_? what was the point of this boolean supposed to be
+        #busy = False # whether we're currently reading a predicament
+        lineNo = 0
+        with open(datadir + '/' + filename, 'r') as fp:
+            for line in fp:
+                lineNo += 1
+                line = line.strip()
+                if line.find("new predicament") == 0:
+                    name = line.split('=')[1]
+                    # create entry in predicaments dictionary
+                    # 'title of predicament' : ('which pred file it is in',
+                    #                         lineNo)
+                    name = name.strip()
+                    predicaments[name] = (filename, lineNo)
+    return predicaments
+
+datadir = os.getcwd() + '/data/predicaments'
+predicaments = findPredicaments(datadir)
 
 if __name__ == '__main__':
     print("content of", datadir, ": ", os.listdir(datadir))
