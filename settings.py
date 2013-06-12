4813e798daeebf3666a8b3f364d8da26741189f6
ninedotnine <warfegle@hotmail.com>
2013-06-12 00:50:51 -0400

    made settings.py smarter

--- settings.py
+++ settings.py
@@ -6,6 +6,7 @@
 actionButtons = '123456'
 #actionButtons = 'aoeujk'
 movementButtons = 'wsad' # up, down, left, right
+#movementButtons = 'jkhl' # up, down, left, right
 
 #fancyPrintSpeed = 0.015
 fancyPrintSpeed = 0.001
@@ -20,7 +21,7 @@ defaultMultilinePrompt = 'Please type something. ^D when done.'
 
 try:
     arrows = '\u2191\u2193\u2190\u2192'
-    print(arrows)
+    print(arrows, file=open('/dev/null','w'))
 except UnicodeEncodeError:
     arrows = '^v<>'
 
@@ -28,3 +29,8 @@ lineLength = 69
 
 soundOn = False
 clearOn = True
+
+if __name__ == '__main__':
+    for thing in dir():
+        if not thing.startswith('__'):
+            print(thing, '=', eval(thing))
